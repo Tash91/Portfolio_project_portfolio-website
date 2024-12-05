b4598e811ce2bae3b4f3c9d4dc169327979c2e14
Natasha Bryant <97158331+Tash91@users.noreply.github.com>
2024-12-05 11:39:03 +0000

    update homepage

--- views.py
+++ views.py
@@ -3,5 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hhome():
-    return render_template('base.html')
+def home():
+    return render_template('home.html')
+
+
+
