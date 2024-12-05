d91f29efe3ca07e69bf9ecf91addb79975041480
Natasha Bryant <97158331+Tash91@users.noreply.github.com>
2024-12-05 14:13:54 +0000

    add second page

--- views.py
+++ views.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/" , methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+       return redirect(url_for('about'))
+         
+
     return render_template('home.html')
 
 
+@app.route("/about-me" , methods=['GET', 'POST'])
+def about():
+    return render_template('about_me.html')
 
